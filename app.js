4f1e69fee41ccaf9fd745d07e0f68e8a4febaade
charlestakang <charles.takang@gmail.com>
2023-03-30 16:16:02 +0000

    Got the list working

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
 const shoppingInput = document.getElementById("shopping-input")
 const addItemBtn = document.getElementById("add-item-btn")
-const extrabtn = document.querySelector(".extra-btn")
+const ul = document.querySelector(".extra-btn")
 // function addToList(){
 //     console.log(`I was clicked`)
 // }
@@ -24,32 +24,128 @@ addItemBtn.addEventListener("click", (e) => {
         alert("Please add an Item")
         return
     }
-
-    if (shoppingInput.value !== null){
-        createItemBtn(shoppingInput.value)
-        return
-    }
+    
+    ul.appendChild(lineItem(shoppingInput.value))
 
     //create a buton with value from shopping input
     //button to list
 })
 
 //create buttonwithclasses
-function createItemBtn(text){
-    const div = document.createElement('div')
-    div.setAttribute('class', 'col-12 col-md-6 d-flex')
+// function createItemBtn(text){
+//     const div = document.createElement('div')
+//     div.setAttribute('class', 'col-12 col-md-6 d-flex')
 
-    const button = document.createElement('button')
-    button.setAttribute('class', 'btn bt-lg flex-fill btn-outline-secondary mb-3')
+//     const button = document.createElement('button')
+//     button.setAttribute('class', 'btn bt-lg flex-fill btn-outline-secondary mb-3')
 
-    div.appendChild(button)
+//     div.appendChild(button)
 
-    button.innerHTML = `${text}<i class="fa-solid fa-xmark text-danger"></i>`
+//     button.innerHTML = `${text}<i class="fa-solid fa-xmark text-danger"></i>`
 
-    extrabtn.appendChild(div)
+//     extrabtn.appendChild(div)
 
 
+// }
+
+// function createButton(text){
+//     const itemBtn = document.createElement('button')
+//     //const tn = document.createTextNode(text)
+//     itemBtn.classList = 'btn bt-lg flex-fill btn-outline-secondary mb-3'
+//     iconItem = document.createElement('i')
+
+//     iconItem.classList ='fa-solid fa-xmark text-danger'
+//     itemBtn.innerHTML = `${text}<i class="fa-solid fa-xmark text-danger"></i>`
+//     return itemBtn
+// }
+
+// function liColumn(text){
+//     const li = document.createElement('li')
+//     li.classList = 'class', 'col-12 col-md-6 d-flex'
+//     li.style.listStyle = 'none'
+//     li.appendChild(createButton(text))
+//     return li
+// }
+
+// function createButton(classes){
+//     const button = document.createElement('button')
+//     button.classList = classes
+
+//     const icon = createIcon("fa-solid fa-xmark text-danger")
+//     button.appendChild(icon)
+
+
+//     return button
+// }
+
+
+// function createIcon(classes){
+//     const icon = document.createElement('i')
+//     icon.className = classes
+
+//     return icon
+// }
+
+
+// function createNewItem(item) {
+//     const li = document.createElement('li')
+//     li.appendChild(document.createTextNode(item))
+
+//     //add button and icon
+//     const button = createButton('btn bt-lg flex-fill btn-outline-secondary mb-3')
+//     li.appendChild(button)
+
+//     ul.appendChild(li)
+// }
+
+function iconItem(){
+    const icon = document.createElement('i')
+    icon.className = 'fa-solid fa-xmark text-danger'
+    return icon
 }
+
+function btnItem(){
+    const button = document.createElement('button')
+    button.setAttribute('type', 'button')
+    button.className = 'btn'
+    button.appendChild(iconItem())
+    return button
+}
+
+function textItem(asignedText){
+    const p = document.createElement('p')
+    p.className = 'pt-2'
+    const text = document.createTextNode(asignedText)
+    p.appendChild(text)
+    
+    return p
+}
+
+function lineItem(text){
+    const li = document.createElement('li')
+    li.className = 'd-flex justify-content-between fw-bold col-12 col-md-6 border px-3 rounded-3 mb-4'
+    li.appendChild(textItem(text))
+    li.appendChild(btnItem())
+    
+    return li
+}
+
+//     function divItem (text){
+//     const div = document.createElement('div')
+//     div.className = 'col-12 col-md-6 border px-3 rounded-3'
+//     div.appendChild(lineItem(text))
+//     return div
+// }
+
+// const ulItem = (text) => {
+//     const ul = document.createElement('ul')
+//     const text = document.createTextNode(text)
+//     ul.className = 'row btn-row extra-btn px-2'
+
+
+
+// }
+
 
 //iterarate over list to display items
 //if list is nothing then display blank
